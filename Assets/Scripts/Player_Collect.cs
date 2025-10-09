267bd09c01aeeede85eda065609575db1f94a278
Krustycrabe <lucas.jbrt13@gmail.com>
2025-10-09 09:37:41 +0200

    [LJO] HUD

--- Assets/Scripts/Player_Collect.cs
+++ Assets/Scripts/Player_Collect.cs
@@ -1,11 +1,17 @@
+using System;
 using UnityEngine;
 
 public class Player_Collect : MonoBehaviour
 {
     [SerializeField] private ScoreDatas _scoreData;
+    [SerializeField] private UIController _uiController;
 
+    public static Action<int> OnTargetCollected;
     public void UpdateScore(int value)
     {
-        _scoreData.ScoreValue = Mathf.Clamp(_scoreData.ScoreValue+ value,0,_scoreData.ScoreValue+ value);
+        _scoreData.ScoreValue = Mathf.Clamp(_scoreData.ScoreValue + value, min: 0, max: _scoreData.ScoreValue + value);
+        _uiController.UpdateScore(_scoreData.ScoreValue);
     }
+      
+    
 }
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _scoreText;
+
+    private void Start()
+    {
+        UpdateScore(0);
+    }
+    
+      
+    
+
+    public void UpdateScore(int newScore)
+    {
+        _scoreText.text = "Score :" + newScore.ToString();
+    }
+      
+
+}
